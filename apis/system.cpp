a27c7f11ff214615f0cac2be53e2d35bfca779b4
payonel <payonel@hotmail.com>
2017-02-09 02:01:09 -0800

    stub methods for system

--- apis/system.cpp
+++ apis/system.cpp
@@ -2,10 +2,28 @@
 
 SystemApi::SystemApi() : LuaProxy("system")
 {
+    add("allowGC", &SystemApi::allowGC);
+    add("timeout", &SystemApi::timeout);
+    add("allowBytecode", &SystemApi::allowBytecode);
 }
 
 SystemApi* SystemApi::get()
 {
     static SystemApi it;
     return &it;
+}
+
+ValuePack SystemApi::allowGC(const ValuePack& args)
+{
+    return ValuePack();
+}
+
+ValuePack SystemApi::timeout(const ValuePack& args)
+{
+    return ValuePack();
+}
+
+ValuePack SystemApi::allowBytecode(const ValuePack& args)
+{
+    return ValuePack();
 }
--- apis/system.h
+++ apis/system.h
@@ -5,6 +5,10 @@ class SystemApi : public LuaProxy
 {
 public:
     static SystemApi* get();
+
+    ValuePack allowGC(const ValuePack& args);
+    ValuePack timeout(const ValuePack& args);
+    ValuePack allowBytecode(const ValuePack& args);
 private:
     SystemApi();
 };
