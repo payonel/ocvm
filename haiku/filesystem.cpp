443329c7c4f33b75ebe474d0624c80d4a41a7d6e
payonel <payonel@hotmail.com>
2018-11-22 01:52:01 -0800

    implement filesystem code (haiku support)

--- haiku/filesystem.cpp
+++ haiku/filesystem.cpp
@@ -6,49 +6,159 @@
 #include <unistd.h>
 
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 namespace haiku
 {
     namespace filesystem
     {
-        void run_cmd(const std::string& command, std::error_code& ec)
+        std::string name(const std::string& path)
         {
-            int result = ::system(command.c_str());
-            ec.assign(result, std::system_category());
-            if (result != 0)
+            if (!path.empty())
             {
-                ec.assign(1, std::system_category());
+                auto last_slash = path.find_last_of("/");
+                if (last_slash != std::string::npos)
+                {
+                    std::string after_slash = path.substr(last_slash + 1);
+                    if (after_slash.empty())
+                    {
+                        // path ended in slash, such as /a/b/c/
+                        return haiku::filesystem::name(path.substr(0, last_slash));
+                    }
+                    // part after last slash not empty, and thus is name
+                    return after_slash;
+                }
+                // no slash, path is whole name
             }
+
+            return path;
+        }
+
+        std::string ensure_slash(const std::string& path)
+        {
+            if (path.empty() || path.at(path.size() - 1) == '/')
+                return path;
+            return path + "/";
+        }
+
+        bool copy_file(const std::string& from, const std::string& to, std::error_code& ec)
+        {
+            ec.assign(1, std::system_category());
+            std::ifstream in(from);
+            if (!in)
+                return false;
+
+            std::ofstream out(to);
+            if (!out)
+                return false;
+
+            out << in.rdbuf();
+            in.close();
+            out.close();
+
+            ec.assign(0, std::system_category());
+            return true;
         }
 
         void copy(const std::string& from, const std::string& to, copy_options options, std::error_code& ec) noexcept
         {
-            std::string command = "cp  ";
-            command += "\"";
-            command += from;
-            command += "\" \"";
-            command += to;
-            command += "\"";
-            run_cmd(command, ec);
+            if (!exists(from, ec))
+                return;
+
+            bool from_is_dir = haiku::filesystem::is_directory(from, ec);
+
+            std::string to_target = to;
+            if (exists(to, ec))
+            {
+                if (is_directory(to, ec))
+                {
+                    to_target = ensure_slash(to) + name(from);
+                }
+                else
+                {
+                    if (from_is_dir)
+                    {
+                        ec.assign(1, std::system_category());
+                        return; // cannot copy dir to file
+                    }
+                }
+            }
+
+            if (from_is_dir)
+            {
+                haiku::filesystem::create_directories(to_target, ec);
+                if (ec.value() != 0)
+                    return;
+
+                for (const auto& ele : directory_iterator(from, ec))
+                {
+                    std::string from_file = ensure_slash(from) + ele.path();
+                    std::string to_file_target = ensure_slash(to_target) + ele.path();
+                    if (!copy_file(from_file, to_file_target, ec))
+                        return;
+                }
+            }
+            else
+            {
+                copy_file(from, to_target, ec);
+            }
         }
 
         void rename(const std::string& from, const std::string& to, std::error_code& ec) noexcept
         {
-            std::string command = "mv ";
-            command += "\"";
-            command += from;
-            command += "\" \"";
-            command += to;
-            command += "\"";
-            run_cmd(command, ec);
+            ec.assign(1, std::system_category());
+            if (haiku::filesystem::exists(to, ec) || !haiku::filesystem::exists(from, ec))
+                return;
+
+            haiku::filesystem::copy(from, to, copy_options::skip_existing, ec);
+            if (ec.value() != 0)
+                return;
+
+            if (!haiku::filesystem::remove(from, ec))
+                ec.assign(1, std::system_category());
+        }
+
+        std::vector<std::string> path_segments(const std::string& path)
+        {
+            std::vector<std::string> vec;
+
+            size_t last = 0;
+            while (last < path.size())
+            {
+                size_t next = path.find("/", last);
+                size_t len = (next == std::string::npos ? path.size() : next) - last;
+                std::string part = path.substr(last, len);
+                vec.push_back(part);
+                last += len + 1;
+            }
+
+            return vec;
         }
 
         void create_directories(const std::string& path, std::error_code& ec) noexcept
         {
-            std::string command = "mkdir -p \"";
-            command += path;
-            command += "\"";
-            run_cmd(command, ec);
+            auto segments = path_segments(path);
+            std::string next;
+
+            for (const auto& part : segments)
+            {
+                next += part + "/";
+                DIR* dir = ::opendir(next.c_str());
+                if (dir)
+                {
+                    ::closedir(dir);
+                }
+                else
+                {
+                    if (::mkdir(next.c_str(), 0755) == -1)
+                    {
+                        ec.assign(1, std::system_category());
+                        return;
+                    }
+                }
+            }
+            ec.assign(0, std::system_category());
         }
 
         bool exists(const std::string& path, std::error_code& ec) noexcept
@@ -89,7 +199,9 @@ namespace haiku
 
         file_time_type last_write_time(const std::string& path, std::error_code& ec) noexcept
         {
-            file_time_type ft(std::chrono::seconds(0));
+            struct ::stat attr;
+            ::stat(path.c_str(), &attr);
+            file_time_type ft(std::chrono::seconds(attr.st_mtime));
             return ft;
         }
 
@@ -110,8 +222,16 @@ namespace haiku
         {
             std::string cmd = "rm -rf ";
             cmd += path;
-            run_cmd(cmd, ec);
-            return ec.value() == 0;
+
+            int result = ::system(cmd.c_str());
+            ec.assign(result, std::system_category());
+            if (result != 0)
+            {
+                ec.assign(1, std::system_category());
+                return false;
+            }
+
+            return true;
         }
 
         std::string current_path(std::error_code& ec) noexcept
