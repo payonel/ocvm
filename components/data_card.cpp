43ed5244ab62561565ceeb0dd22f0a8165ca1623
Amanda Cameron <amanda@darkdna.net>
2019-08-01 15:30:46 -0400

    Fix formatting, add .clang-format

--- components/data_card.cpp
+++ components/data_card.cpp
@@ -2,13 +2,16 @@
 
 #include "model/log.h"
 #include "model/host.h"
+
 #include "util/crc32.h"
+#include "util/md5.h"
 
 bool DataCard::s_registered = Host::registerComponentType<DataCard>("data");
 
 DataCard::DataCard()
 {
     add("crc32", &DataCard::crc32);
+    add("md5", &DataCard::md5);
 }
 
 DataCard::~DataCard() {};
@@ -26,4 +29,10 @@ int DataCard::crc32(lua_State* lua) {
     vector<char> value = Value::checkArg<vector<char>>(lua, 1);
 
     return ValuePack::ret(lua, util::crc32(value));
+}
+
+int DataCard::md5(lua_State* lua) {
+    vector<char> value = Value::checkArg<vector<char>>(lua, 1);
+    
+    return ValuePack::ret(lua, util::md5(value));
 }
